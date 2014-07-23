1ff8b8c94638930b4af3f05d6665fd11606679ec
vipnormalhy <vipnormalhy@gmail.com>
2014-07-23 19:47:05 +0800

    learn variable definition

--- c++/variable_init.cpp
+++ c++/variable_init.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 
+double calc(float a, float b)
+{
+	return (double)a+b;
+}
+
 int main()
 {
 	int lang;
+	double calc = calc(4.0, 5.0);
 	std::cin >> lang;
 
 	std::cout << lang << std::endl;
