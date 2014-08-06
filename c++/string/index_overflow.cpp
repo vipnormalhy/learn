856528aca39e8570b081e224dad702bc6a7a051b
vipnormalhy <vipnormalhy@gmail.com>
2014-08-06 18:20:25 +0800

    add comment for index overflow

--- c++/string/index_overflow.cpp
+++ c++/string/index_overflow.cpp
@@ -5,6 +5,7 @@ int main()
 {
 	std::string a("I am a handsome boy!\n");
 
+	// the behivor is undefined
 	for (std::string::size_type index = 0; index < 30; index++) 
 	{
 		std::cout << "The size of string is " << a.size() << " now index is " << index << " now char is " << a[index] << std::endl;
