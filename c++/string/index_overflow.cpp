ef75c511a175a2dbb881abe2ca3d62c8ca4dd413
vipnormalhy <vipnormalhy@gmail.com>
2014-08-06 18:15:01 +0800

    add string overflow code

---
+++ c++/string/index_overflow.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::string a("I am a handsome boy!\n");
+
+	for (std::string::size_type index = 0; index < 30; index++) 
+	{
+		std::cout << "The size of string is " << a.size() << " now index is " << index << " now char is " << a[index] << std::endl;
+	}
+	return 1;
+}
