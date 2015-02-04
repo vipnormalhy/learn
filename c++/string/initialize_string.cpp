57511f7a154186b75d7f4158e61bf4c6eb6b70d1
vipnormalhy <vipnormalhy@gmail.com>
2015-02-05 01:03:49 +0800

    try to know if memory address has changed when cat a string

--- c++/string/initialize_string.cpp
+++ c++/string/initialize_string.cpp
@@ -6,10 +6,19 @@ using namespace::std;
 int main()
 {
 	string a(10, 'c');
+	string *pStr;
 
 	cout << a << endl;
 
 	cout << a.size() << endl;
+
+	pStr = &a;
+	cout << pStr << endl;
+
+	a += "ddddd";
+
+	pStr = &a;
+	cout << pStr << endl;
 
 	return 1;
 }
