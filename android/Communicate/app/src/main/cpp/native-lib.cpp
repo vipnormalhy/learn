94a13f0470e8b3da136520c6e99970119d81b21d
normalhy <vipnormalhy@gmail.com>
2018-02-27 16:50:57 +0800

    initialize android project code

--- android/Communicate/app/src/main/cpp/native-lib.cpp
+++ android/Communicate/app/src/main/cpp/native-lib.cpp
@@ -2,9 +2,11 @@
 #include <string>
 
 extern "C"
-JNIEXPORT jstring JNICALL
+JNIEXPORT jstring
+
+JNICALL
 Java_communicate_normal_smallgames_com_communicate_MainActivity_stringFromJNI(
-        JNIEnv* env,
+        JNIEnv *env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
