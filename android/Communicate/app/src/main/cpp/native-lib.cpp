df075d040c39e83efeb03bc737db45f834fc0ea0
normalhy <vipnormalhy@gmail.com>
2018-02-27 16:47:05 +0800

    add initial communicate code

---
+++ android/Communicate/app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,11 @@
+#include <jni.h>
+#include <string>
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_communicate_normal_smallgames_com_communicate_MainActivity_stringFromJNI(
+        JNIEnv* env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
