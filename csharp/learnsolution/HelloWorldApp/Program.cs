b27a7d851e49ce802ab91ef5d49461a8b592be55
normalhy <vipnormalhy@gmail.com>
2018-06-10 00:55:02 +0800

    test csharp GetType function, test visual studio debug arguments property

--- csharp/learnsolution/HelloWorldApp/Program.cs
+++ csharp/learnsolution/HelloWorldApp/Program.cs
@@ -7,6 +7,21 @@ namespace HelloWorldApp
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Hello World!");
+
+			var name = "Test string";
+			var age = 25;
+			var is_bool = true;
+			Type name_type = name.GetType();
+			Type age_type = age.GetType();
+			Type bool_type = is_bool.GetType();
+
+			Console.WriteLine($"name type is {name_type}");
+			Console.WriteLine($"age type is {age_type}");
+			Console.WriteLine($"bool type is {bool_type}");
+
+			for (int i = 0; i < args.Length; i++) {
+				Console.WriteLine("args: " + args[i]);
+			}
 		}
 	}
 }
