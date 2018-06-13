f9927dac76acc2e18c572d62e5755e1b2943b7bf
normalhy <vipnormalhy@gmail.com>
2018-06-13 11:09:58 -0700

    add allocate Heap memory test

---
+++ csharp/UnmanagedSolution/UnmanagedCode/Interface/HeapMallocInterface.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace UnmanagedCode.Interface {
+	class HeapMallocInterface {
+		unsafe public static IntPtr HeapAllocate(int size) {
+				if (size <= 0) {
+					return IntPtr.Zero;
+				}
+
+				return Marshal.AllocHGlobal(size);
+		}
+
+		public static void HeapFree(IntPtr pointer) {
+				Marshal.FreeHGlobal(pointer);
+		}
+	}
+}
--- csharp/UnmanagedSolution/UnmanagedCode/Program.cs
+++ csharp/UnmanagedSolution/UnmanagedCode/Program.cs
@@ -6,10 +6,19 @@ using System.Threading.Tasks;
 
 namespace UnmanagedCode
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
+	class Program {
+		static void Main(string[] args) {
+			// test
+			Test();
+		}
+
+		static void Test() {
+			TestCode.TestCase testcase = new TestCode.TestCase();
+
+			testcase.TestHeapLeak(1000);
+			testcase.TestHeapUse();
+			Console.WriteLine("Press any key to exit");
+			Console.ReadLine();
 		}
 	}
 }
