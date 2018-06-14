21211be99e5f1c55a8a18f1f35e63a10394a832a
normalhy <vipnormalhy@gmail.com>
2018-06-13 21:06:06 -0700

    add test dispose function

--- csharp/UnmanagedSolution/UnmanagedCode/Program.cs
+++ csharp/UnmanagedSolution/UnmanagedCode/Program.cs
@@ -8,16 +8,28 @@ namespace UnmanagedCode
 {
 	class Program {
 		static void Main(string[] args) {
+
 			// test
 			Test();
+			TestGCUnsafe();
 		}
 
 		static void Test() {
 			TestCode.TestCase testcase = new TestCode.TestCase();
 
+			// test heap malloc
 			testcase.TestHeapLeak(1000);
 			testcase.TestHeapUse();
-			Console.WriteLine("Press any key to exit");
+
+			Console.WriteLine("Press any key to exit Heap test");
+			Console.ReadLine();
+		}
+
+		static void TestGCUnsafe() {
+			TestCode.TestCase testcase = new TestCode.TestCase();
+
+			testcase.TestGCUnsafe();
+			Console.WriteLine("Press any key to exit unmanaged memory test");
 			Console.ReadLine();
 		}
 	}
