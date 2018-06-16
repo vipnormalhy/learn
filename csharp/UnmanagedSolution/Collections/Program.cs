7730371e9b53f920b3d9ef3ec364587d3ef68850
normalhy <vipnormalhy@gmail.com>
2018-06-15 17:20:53 -0700

    learn compare interface

--- csharp/UnmanagedSolution/Collections/Program.cs
+++ csharp/UnmanagedSolution/Collections/Program.cs
@@ -10,6 +10,17 @@ namespace Collections
 		static void Main(string[] args) {
 			ArrayTest();
 			JaggedTest();
+			CreateArray();
+			TestCompare();
+		}
+
+		static void CreateArray() {
+			Array a = Array.CreateInstance(typeof(int), 10);
+			a.SetValue(1, 0);
+
+			foreach (var i in a) {
+				Console.WriteLine($"value is {i}");
+			}
 		}
 
 		static void JaggedTest() {
@@ -20,6 +31,8 @@ namespace Collections
 			foreach (var i in a) {
 				Console.WriteLine($"value is {i}");
 			}
+
+			Console.WriteLine($"array range is {a.Rank}");
 		}
 
 		static void ArrayTest() {
@@ -37,6 +50,37 @@ namespace Collections
 
 			Console.WriteLine($"value length is {b.Length}");
 			// Console.WriteLine($"value[5] is ${b[5]}"); // Don't Support
+			Console.WriteLine($"array range is {a.Rank}, array b's range is {b.Rank}");
+		}
+
+		class Person: IComparable<Person> {
+			public string name_;
+
+			public Person(string name) {
+				name_ = name;
+			}
+
+			public int CompareTo(Person other) {
+				if (other == null) {
+					return -1;
+				}
+
+				return name_.CompareTo(other.name_);
+			}
+		}
+
+		static void TestCompare() {
+			Person[] persons = new Person[] {
+				new Person("bca"), new Person("bbbbb"), new Person("acb"),
+			};
+
+			Array.Sort(persons);
+
+			Console.WriteLine($"data is {persons}");
+
+			foreach (var i in persons) {
+				Console.WriteLine($"data is {i.name_}");
+			}
 		}
 	}
 }
