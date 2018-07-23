ee7b05ea7d58b81f2834006d2d10828b4ccb56e4
vipnormalhy <vipnormalhy@gmail.com>
2018-07-23 23:26:24 +0800

    fix signal handle failed

--- boost/stacktrace/learn_stacktrace.cpp
+++ boost/stacktrace/learn_stacktrace.cpp
@@ -1,13 +1,10 @@
 #include <iostream>
 #include <stdio.h>
-#include <signal.h>
+#include <csignal>
 #include <sys/types.h>
 #include <boost/thread.hpp>
 #include <boost/chrono.hpp>
 
-int signal_handler(int sig_num) {
-	std::cout << "Receive signal " << sig_num << std::endl;
-}
 
 void signal_action_handler(int sig_num, siginfo_t *info, void *data) {
 	std::cout << "Receive signum " << sig_num << std::endl;
@@ -16,10 +13,9 @@ void signal_action_handler(int sig_num, siginfo_t *info, void *data) {
 
 int main() {
 	struct sigaction sa;
+	sa.sa_flags = SA_SIGINFO | SA_RESETHAND;
 	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = SA_SIGINFO;
 	sa.sa_sigaction = signal_action_handler;
-	sa.sa_handler = NULL;
 	sa.sa_restorer = NULL;
 
 	sigaction(SIGINT, &sa, NULL);
