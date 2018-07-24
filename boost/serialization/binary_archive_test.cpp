acc696f28a65a074bcde63fe613c7f964e5d28f2
vipnormalhy <vipnormalhy@gmail.com>
2018-07-24 23:44:18 +0800

    add text archive code

--- boost/serialization/binary_archive_test.cpp
+++ boost/serialization/binary_archive_test.cpp
@@ -1,7 +1,11 @@
 #include <sstream>
 #include <iostream>
+#include <fstream>
+#include <string>
 #include <boost/archive/binary_oarchive.hpp>
 #include <boost/archive/binary_iarchive.hpp>
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
 
 class CEntity {
 	private:
@@ -23,19 +27,77 @@ class CEntity {
 
 BOOST_CLASS_VERSION(CEntity, 0);
 
-int main() {
+std::ostringstream &pack_binary_archive(CEntity &entity, std::ostringstream &oss) {
+	boost::archive::binary_oarchive oa(oss);
+	oa << entity;
+	return oss;
+}
+
+CEntity unpack_binary_archive(std::ostringstream &oss) {
+	assert(oss);
+
+	std::istringstream iss(oss.str());
+	boost::archive::binary_iarchive ia(iss);
+
+	CEntity entity;
+	ia >> entity;
+	return entity;
+}
+
+
+void binary_archive_test() {
+	printf("Start binary serialization test-------------\n");
+	CEntity monster;
+	monster.mod_hp(100);
+	monster.mod_mp(100);
+	std::ostringstream oss;
+	pack_binary_archive(monster, oss);
+
+	CEntity monster2;
+	std::cout << "hp is " << monster2.get_hp() << std::endl;
+	std::cout << "mp is " << monster2.get_mp() << std::endl;
+	monster2 = unpack_binary_archive(oss);
+	std::cout << "hp is " << monster2.get_hp() << std::endl;
+	std::cout << "mp is " << monster2.get_mp() << std::endl;
+	printf("Stop binary serialization test-------------\n");
+}
+
+
+bool dump_archive_to_file(CEntity &entity, std::string &filename) {
+	std::ofstream ofs(filename.c_str());
+	boost::archive::text_oarchive oa(ofs);
+	oa << entity;
+	ofs.close();
+	return true;
+}
+
+bool restore_from_archive_file(CEntity &entity, std::string &filename) {
+	std::ifstream ifs(filename.c_str());
+	boost::archive::text_iarchive ia(ifs);
+	ia >> entity;
+	return true;
+}
+
+void text_archive_test() {
+	printf("Start text serialization test-------------\n");
 	CEntity monster;
 	monster.mod_hp(100);
 	monster.mod_mp(100);
 
-	std::ostringstream oss;
-	boost::archive::binary_oarchive oa(oss);
-	oa << monster;
-
+	std::string filename("dump_entity.data");
+	dump_archive_to_file(monster, filename);
 	CEntity monster2;
-	std::istringstream iss(oss.str());
-	boost::archive::binary_iarchive ia(iss);
-	ia >> monster2;
-	std::cout << monster2.get_hp() << " mp is " << monster2.get_mp() << std::endl;
+	std::cout << "hp is " << monster2.get_hp() << std::endl;
+	std::cout << "mp is " << monster2.get_mp() << std::endl;
+	restore_from_archive_file(monster2, filename);
+	std::cout << "hp is " << monster2.get_hp() << std::endl;
+	std::cout << "mp is " << monster2.get_mp() << std::endl;
+	printf("Stop text serialization test-------------\n");
+}
+
+
+int main() {
+	binary_archive_test();
+	text_archive_test();
 	return 0;
 }
