b9e457df334357680fbb5385772651673beed6cf
Normal He <vipnormalhy@gmail.com>
2018-07-25 07:26:29 +0000

    fix serialize bug

--- boost/serialization/archive_test.cpp
+++ boost/serialization/archive_test.cpp
@@ -19,12 +19,15 @@ void test_archive() {
 
 void test_member_archive() {
 	CEntityQueue entity_queue;
-	CEntity entity = entity_queue.get_head();
-	entity.mod_hp(100);
-	entity.mod_mp(50);
+	CEntity *entity = entity_queue.get_head();
+	entity->mod_hp(100);
+	entity->mod_mp(50);
 	entity = entity_queue.get_end();
-	entity.mod_hp(1000);
-	entity.mod_mp(30);
+	entity->mod_hp(1000);
+	entity->mod_mp(30);
+
+	std::cout << "head hp is " << entity_queue.get_head()->get_hp() << "head mp is " << entity_queue.get_head()->get_mp() << std::endl;
+	std::cout << "head hp is " << entity_queue.get_end()->get_hp() << "head mp is " << entity_queue.get_end()->get_mp() << std::endl;
 
 	std::stringstream ss;
 	boost::archive::binary_oarchive oa(ss);
@@ -33,8 +36,8 @@ void test_member_archive() {
 	CEntityQueue entity_queue2;
 	boost::archive::binary_iarchive ia(ss);
 	ia >> entity_queue2;
-	std::cout << "head hp is " << entity_queue.get_head().get_hp() << "head mp is " << entity_queue.get_head().get_hp() << std::endl;
-	std::cout << "head hp is " << entity_queue.get_end().get_hp() << "head mp is " << entity_queue.get_end().get_hp() << std::endl;
+	std::cout << "head hp is " << entity_queue2.get_head()->get_hp() << "head mp is " << entity_queue2.get_head()->get_mp() << std::endl;
+	std::cout << "head hp is " << entity_queue2.get_end()->get_hp() << "head mp is " << entity_queue2.get_end()->get_mp() << std::endl;
 }
 
 int main() {
--- boost/serialization/archive_test.hpp
+++ boost/serialization/archive_test.hpp
@@ -31,6 +31,7 @@ void CEntity::save(Archive &ar, const unsigned int version) const {
 	}
 	ar << hp;
 	ar << mp;
+	ar << sp;
 	std::cout << "Enter CEntity save" << std::endl;
 }
 
@@ -115,6 +116,6 @@ class CEntityQueue {
 		CEntity head;
 		CEntity end;
 	public:
-		inline CEntity &get_head() {return head;}
-		inline CEntity &get_end() {return end;}
+		inline CEntity *get_head() {return &head;}
+		inline CEntity *get_end() {return &end;}
 };
