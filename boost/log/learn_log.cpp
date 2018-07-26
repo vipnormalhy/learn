b3da843de8eba3aae87c90b8f813aab579522e5a
Normal He <vipnormalhy@gmail.com>
2018-07-26 12:29:27 +0000

    add boost log learn code

--- boost/log/learn_log.cpp
+++ boost/log/learn_log.cpp
@@ -1,33 +1,33 @@
-#include <boost/thread.hpp>
-#include <boost/stacktrace.hpp>
 #include <boost/log/core.hpp>
 #include <boost/log/trivial.hpp>
 #include <boost/log/expressions.hpp>
-#include <boost/log/sinks/text_file_backend.hpp>
-#include <boost/log/utility/setup/file.hpp>
-#include <boost/log/utility/setup/common_attributes.hpp>
-#include <boost/log/sources/severity_logger.hpp>
-#include <boost/log/sources/record_ostream.hpp>
+#include <boost/log/utility/setup.hpp>
 
-void init() {
-	boost::log::add_file_log(
-			boost::log::keywords::file_name = "sample%N.log",
-			boost::log::keywords::rotation_size = 1024 * 10 * 10,
-			boost::log::keywords::time_based_rotation = boost::log::sinks::file::rotation_at_time_point(0, 0, 0),
-			boost::log::keywords::format = "[%TimeStamp%]: %Message%");
-	boost::log::core::get()->set_filter(
-			boost::log::trivial::severity > boost::log::trivial::info);
+void init_logging() {
+	namespace logging = boost::log;
+	namespace keywords = boost::log::keywords;
+	logging::add_common_attributes();
+
+	logging::add_file_log(
+		keywords::file_name = "sample_%N.log",
+		keywords::rotation_size = 10 * 1024 * 1024,
+		keywords::format = "[%TimeStamp%]: %Message%");
+	logging::core::get()->set_filter(logging::trivial::severity >= logging::trivial::info);
 }
 
-int main() {
-	init();
+int main(int, char *[]) {
+	init_logging();
 
-	boost::stacktrace::stacktrace *st = new boost::stacktrace::stacktrace;
-	BOOST_LOG_TRIVIAL(trace) << *st;
-	BOOST_LOG_TRIVIAL(debug) << *st;
-	BOOST_LOG_TRIVIAL(info) << *st;
-	BOOST_LOG_TRIVIAL(warning) << *st;
-	BOOST_LOG_TRIVIAL(error) << *st;
-	BOOST_LOG_TRIVIAL(fatal) << *st;
+	namespace boost_trivial = boost::log::trivial;
+	namespace boost_src = boost::log::sources;
+
+	boost_src::severity_logger<boost_trivial::severity_level> lg;
+
+	BOOST_LOG_SEV(lg, boost::log::trivial::trace) << "This is trace message";
+	BOOST_LOG_SEV(lg, boost_trivial::debug) << "This is debug message";
+	BOOST_LOG_SEV(lg, boost_trivial::info) << "This is info message";
+	BOOST_LOG_SEV(lg, boost_trivial::warning) << "This is warning message";
+	BOOST_LOG_SEV(lg, boost_trivial::error) << "This is error message";
+	BOOST_LOG_SEV(lg, boost_trivial::fatal) << "This is fatal message";
 	return 0;
 }
