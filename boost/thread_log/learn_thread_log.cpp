38ce79646c514a911111a1dc43e165fff62ad3a0
Normal He <vipnormalhy@gmail.com>
2018-07-27 07:52:05 +0000

    fix member function thread bug

--- boost/thread_log/learn_thread_log.cpp
+++ boost/thread_log/learn_thread_log.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <boost/bind.hpp>
 #include "learn_thread_log.hpp"
 
 void TestThreadInterrupt::timer_print() {
@@ -20,4 +21,9 @@ void TestThreadInterrupt::timer_print() {
 
 int main(int, char *[]) {
 	TestThreadInterrupt test;
+
+	auto func = boost::bind(&TestThreadInterrupt::timer_print, &test);
+	test.m_t = boost::make_shared<boost::thread>(func);
+	test.m_t->join();
+	std::cout << "thread end." << std::endl;
 }
