078b05a118eefcdbbe575d8aa126ca43ea78b4dd
vipnormalhy <vipnormalhy@gmail.com>
2018-08-03 00:41:25 +0800

    delete global data file

--- boost/thread_log/common.h
+++ boost/thread_log/common.h
@@ -2,11 +2,15 @@
 #ifndef LEARN_THREADS_LOG_COMMON_H
 #define LEARN_THREADS_LOG_COMMON_H
 
+#include "configure.h"
 #include "log_manager/log_manager.hpp"
 
 // global logger
 CLogManager g_logger_manager("common");
 CLogManager::LOGGER_TYPE g_logger = g_logger_manager.get_logger();
 CLogManager::LOGGER_MT_TYPE g_logger_mt = g_logger_manager.get_logger_mt();
+
+// global configuration
+
 
 #endif
