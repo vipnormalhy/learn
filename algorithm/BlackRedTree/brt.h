29a9eb75108c7d26953335982f9a7ca9582ce91f
normalhy <vipnormalhy@gmail.com>
2019-10-02 17:08:18 +0800

    add adjust black red tree

--- algorithm/BlackRedTree/brt.h
+++ algorithm/BlackRedTree/brt.h
@@ -1,6 +1,7 @@
 #include <list>
 #include <memory>
 #include <iostream>
+#include <tuple>
 
 enum BRTreeColor
 {
@@ -16,7 +17,7 @@ public:
   BRTreeColor m_color;
   std::shared_ptr<BRTreeNode<T>> m_left_ptr;
   std::shared_ptr<BRTreeNode<T>> m_right_ptr;
-  std::shared_ptr<BRTreeNode<T>> m_parent_prt;
+  std::shared_ptr<BRTreeNode<T>> m_parent_ptr;
 };
 
 template<typename T>
@@ -34,7 +35,11 @@ private:
 
   void travel_subtree(std::shared_ptr<BRTreeNode<T>> node_ptr);
   bool insert_head(const T &value);
-  std::shared_ptr<BRTreeNode<T>> create_insert_node(const T &value);
+
+  // adjust Red-Black Tree
+  bool adjust_tree(std::shared_ptr<BRTreeNode<T>> start_node);
+  bool adjust_head(std::shared_ptr<BRTreeNode<T>> start_node);
+  std::tuple<std::shared_ptr<BRTreeNode<T>>, std::shared_ptr<BRTreeNode<T>>> create_insert_node(const T &value);
 };
 
 template<typename T>
@@ -58,17 +63,22 @@ bool BRTree<T>::insert(const T &value)
     return insert_head(value);
   }
 
-  auto node = create_insert_node(value);
+  // Insert a node to BRTree
+  std::shared_ptr<BRTreeNode<T>> parent, node;
+  tie(parent, node) = create_insert_node(value);
   if (!node)
   {
     std::cerr << "Cannot find a node for " << value << std::endl;
     std::terminate();
   }
-
   node->m_value = value;
   node->m_color = RED;
+  node->m_parent_ptr = parent;
   node->m_left_ptr = nullptr;
   node->m_right_ptr = nullptr;
+
+  // adjust red-black
+  adjust_tree(node);
   return true;
 }
 
@@ -80,6 +90,7 @@ bool BRTree<T>::insert_head(const T &value)
   m_brtree_ptr->m_color = BLACK;
   m_brtree_ptr->m_left_ptr = nullptr;
   m_brtree_ptr->m_right_ptr = nullptr;
+  m_brtree_ptr->m_parent_ptr = nullptr;
   return true;
 }
 
@@ -112,12 +123,12 @@ void BRTree<T>::travel_subtree(std::shared_ptr<BRTreeNode<T>> node_ptr)
 }
 
 template<typename T>
-std::shared_ptr<BRTreeNode<T>> BRTree<T>::create_insert_node(const T &value)
+std::tuple<std::shared_ptr<BRTreeNode<T> >, std::shared_ptr<BRTreeNode<T>> > BRTree<T>::create_insert_node(const T &value)
 {
   auto node = m_brtree_ptr;
   if (node == nullptr)
   {
-    return node;
+    return std::make_tuple(nullptr, node);
   }
 
   while(true)
@@ -131,7 +142,7 @@ std::shared_ptr<BRTreeNode<T>> BRTree<T>::create_insert_node(const T &value)
       else
       {
         node->m_right_ptr = std::make_shared<BRTreeNode<T>>();
-        return node->m_right_ptr;
+        return std::make_tuple(node, node->m_right_ptr);
       }
     }
     else
@@ -143,8 +154,29 @@ std::shared_ptr<BRTreeNode<T>> BRTree<T>::create_insert_node(const T &value)
       else
       {
         node->m_left_ptr = std::make_shared<BRTreeNode<T>>();
-        return node->m_left_ptr;
+        return std::make_tuple(node, node->m_left_ptr);
       }
     }
   }
+}
+
+template<typename T>
+bool BRTree<T>::adjust_tree(std::shared_ptr<BRTreeNode<T>> start_node)
+{
+  if (!start_node->m_parent_ptr)
+  {
+    return adjust_head(start_node);
+  }
+  return true;
+}
+
+template<typename T>
+bool BRTree<T>::adjust_head(std::shared_ptr<BRTreeNode<T>> start_node)
+{
+  if (start_node->m_color != BLACK)
+  {
+    start_node->m_color = BLACK;
+  }
+
+  return true;
 }
